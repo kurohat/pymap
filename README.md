8485ea0f150d57f20c6e4b5f56476df3702f3b11
amata a <ch15mt.ag@gmail.com>
2020-08-01 19:36:52 +0200

    v 0.2
    added samba and nfs scan

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ pymap = python3 + nmap. Im so fucking tried of copy+paste port number
 - argparse
 
 # usage
-```py
-python3 pymap.py -h
+```console
+$ python3 pymap.py -h
+$ python3 pymap.py -t $IP # scan top 10k port
+$ python3 pymap.py -t $IP -all # scan every port
+$ python3 pymap.py -t $IP -smb # samba scan
+$ python3 pymap.py -t $IP -nfs # nfs scan
 ```
 # todo:
-- [ ] samba: `nmap -p 445 --script=smb-enum-shares.nse,smb-enum-users.nse <ip>`
-- [ ] rpcbind: `nmap -p 111 --script=nfs-ls,nfs-statfs,nfs-showmount <ip>`
-- [ ] improve speed
+- [x] samba: `nmap -p 445 --script=smb-enum-shares.nse,smb-enum-users.nse <ip>`
+- [x] rpcbind: `nmap -p 111 --script=nfs-ls,nfs-statfs,nfs-showmount <ip>`
+- [ ] improve speed (using thread?)
--- pymap.py
+++ pymap.py
@@ -1,4 +1,4 @@
-import subprocess, argparse
+import subprocess, argparse, sys
 
 intro = """created by gu2rks/kurohat \nfind me here https://github.com/gu2rks
 """
@@ -8,11 +8,15 @@ def parse():
     parser = argparse.ArgumentParser(
         description="pymap = python3 + nmap. Im so fucking tried of copy+paste port number")
     parser.add_argument('-t', '--target', type=str,
-                        required=True, help='target ip')
+                        required=True, help='Target ip')
     parser.add_argument('-s', '--script', type=str,
                         help="[Optional] Name of script")
     parser.add_argument('-all', '--all', action="store_true",
-                        help="[Optional]scann all ports, only scan top 10000 ports if this is not given")
+                        help="[Optional] scann all ports, only scan top 10000 ports if this is not given")
+    parser.add_argument('-smb', '--samba', action="store_true",
+                        help="[Optional] nmap smb script at port 445")
+    parser.add_argument('-nfs', '--nfs', action="store_true",
+                        help="[Optional] nmap nsf script at port 111")
     return parser.parse_args()
 
 args = parse()
@@ -20,25 +24,34 @@ target = args.target
 port = None
 script = None
 
-if args.all:
-    port = '-p-'
-if args.script:
-    script = args.script
+if args.samba or args.nfs is True: #samba or nfs scan
+    if args.samba is True:
+        output = subprocess.getoutput('nmap %s -Pn -p 445 --script=smb-enum-shares.nse,smb-enum-users.nse -T5' % (target))
+    else:
+        output = subprocess.getoutput('nmap %s -Pn -p 111 --script=nfs-ls,nfs-statfs,nfs-showmount -T5' % (target))
+    print(output)
+    sys.exit(0)
 
-print('port scanning...')
-if port is None:
-    output = subprocess.getoutput('nmap %s -T5 -Pn| grep "/" | grep -v "https://nmap.org"' % (target))
-else:
-    out = subprocess.getoutput('nmap %s %s -T5 -Pn| grep "/" | grep -v "https://nmap.org"' % (target, port))
-print(output)
-port = ''
-for line in str(output).splitlines():
-    port = port+line.split('/')[0]+','
+else: 
+    if args.all:
+        port = '-p-'
+    if args.script:
+        script = args.script
 
-print('Enumerating open ports...')
-if script is None:
-    output = subprocess.getoutput('nmap %s -p%s -Pn -A -O -v -T5' % (target, port))
-else:
-    output = subprocess.getoutput('nmap %s -p%s -A -O -Pn -T5 -v --script %s' % (target, port, script))
+    print('port scanning...')
+    if port is None:
+        output = subprocess.getoutput('nmap %s -T5 -Pn| grep "/" | grep -v "https://nmap.org"' % (target))
+    else:
+        out = subprocess.getoutput('nmap %s %s -T5 -Pn| grep "/" | grep -v "https://nmap.org"' % (target, port))
+    print(output)
+    port = ''
+    for line in str(output).splitlines():
+        port = port+line.split('/')[0]+','
 
-print(output)
+    print('Enumerating open ports...')
+    if script is None:
+        output = subprocess.getoutput('nmap %s -p%s -Pn -A -O -T5' % (target, port))
+    else:
+        output = subprocess.getoutput('nmap %s -p%s -A -O -Pn -T5 --script %s' % (target, port, script))
+
+    print(output)
